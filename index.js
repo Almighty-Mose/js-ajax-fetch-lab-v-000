ab8e349dacbe4badc37c6d1d4757e566029eca7d
Branden Mosley <branden.mosley1337@gmail.com>
2018-09-19 21:10:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function forkRepo() {
 
 function showResults(json) {
   //use this function to display the results from forking via the API
-  document.getElementById('')
+  document.getElementById('#results')
 }
 
 function createIssue() {
